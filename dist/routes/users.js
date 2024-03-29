f447bfd4b1f9e989ccb52b4ccb573fd5d770bfc5
yurats <tsymborovychyurii@gmail.com>
2024-03-29 21:45:15 +0200

    last commit

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -16,25 +16,12 @@ function verifyToken(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         var _a, _b;
         const token = (_b = (_a = req.cookies) === null || _a === void 0 ? void 0 : _a.jwtToken) === null || _b === void 0 ? void 0 : _b.token;
-        //const expiresIn = req.cookies?.jwtToken?.expires;
-        //console.log(expiresIn);
         if (token) {
             const [, accessToken] = token.split(' ');
             const decodedToken = jsonwebtoken.decode(accessToken, { complete: true });
             const sub = decodedToken === null || decodedToken === void 0 ? void 0 : decodedToken.payload.sub;
-            const expires = decodedToken === null || decodedToken === void 0 ? void 0 : decodedToken.payload.exp;
-            const expiresInMiliseconds = expires * 1000;
-            let date = Date.now();
-            console.log(date - expiresInMiliseconds);
-            console.log(expiresInMiliseconds);
-            console.log(date);
             const neededUser = yield User.findOne({ _id: sub });
             if (neededUser) {
-                // if(date > expiresInMiliseconds){
-                //     console.log(date>expires)
-                //     res.clearCookie('jwtToken');
-                //     res.redirect('/login');
-                // }
                 next();
             }
             else {
@@ -62,14 +49,34 @@ router.post('/login', (req, res, next) => __awaiter(void 0, void 0, void 0, func
         const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
         if (isValid) {
             const tokenObject = utils.issueJWT(user);
-            //const refresh = utils.issueRefresh(user);
-            //res.cookie('refreshToken', refresh, { httpOnly: true, secure: true });
-            res.cookie('jwtToken', tokenObject, { maxAge: 20 * 1000, httpOnly: true, secure: true });
+            const refreshObject = utils.issueRefresh(user);
+            res.cookie('jwtToken', tokenObject, { maxAge: 60 * 1000, httpOnly: true, secure: true });
+            res.cookie('refreshToken', refreshObject, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true });
             res.status(200).redirect("/add-holiday");
         }
         else {
             console.log("Password entered incorrectly!");
             return res.status(401).json({ success: false, msg: "You entered the wrong password" });
+        }
+    }
+    catch (err) {
+        return next(err);
+    }
+}));
+router.post('/refresh-jwt', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c, _d;
+    try {
+        const token = (_b = (_a = req.cookies) === null || _a === void 0 ? void 0 : _a.jwtToken) === null || _b === void 0 ? void 0 : _b.token;
+        const refreshToken = (_d = (_c = req.cookies) === null || _c === void 0 ? void 0 : _c.refreshToken) === null || _d === void 0 ? void 0 : _d.token;
+        const [, accessToken] = token.split(' ');
+        const decodedToken = jsonwebtoken.decode(accessToken, { complete: true });
+        const sub = decodedToken === null || decodedToken === void 0 ? void 0 : decodedToken.payload.sub;
+        const neededUser = yield User.findOne({ _id: sub });
+        console.log(neededUser);
+        if (refreshToken) {
+            const tokenObject = utils.issueJWT(neededUser);
+            res.cookie('jwtToken', tokenObject, { maxAge: 60 * 1000, httpOnly: true, secure: true });
+            res.redirect('/add-holiday');
         }
     }
     catch (err) {
@@ -91,7 +98,6 @@ router.post('/register', (req, res, next) => {
     newUser.save()
         .then((user) => {
         const jwt = utils.issueJWT(user);
-        //const refresh = utils.issueRefresh(user);
         res.status(200).redirect("/login");
     })
         .catch(err => next(err));
@@ -100,29 +106,4 @@ router.post('/logout', (req, res, next) => {
     res.clearCookie('jwtToken');
     res.redirect('/login');
 });
-// router.post('/refresh', verifyToken, async (req, res, next) => {
-//     try {
-//         const user = await User.findOne({username: req.body.username});
-//
-//         if(!user) {
-//             return res.status(401).json({success: false, msg: "Could not find user"});
-//         }
-//
-//         const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
-//
-//         if(isValid){
-//             const tokenObject = utils.issueJWT(user);
-//             const refresh = utils.issueRefresh(user);
-//             res.cookie('refreshToken', refresh, { httpOnly: true, secure: true });
-//             res.cookie('jwtToken', tokenObject, { httpOnly: true, secure: true });
-//             res.status(200).redirect("/add-holiday");
-//         } else {
-//             console.log("Password entered incorrectly!");
-//             return res.status(401).json({success: false, msg: "You entered the wrong password"});
-//         }
-//     } catch (err) {
-//         return next(err);
-//     }
-// });
-//export default router;
 export { router, verifyToken, };
